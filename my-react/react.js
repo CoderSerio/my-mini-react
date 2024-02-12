2104fe8c62aa828680e144740c7b571713839f14
CoderSerio <2779066456@qq.com>
2024-02-12 18:37:07 +0800

    step7: 进一步完成workLoop结构, 即将建立fiber树（部分代码未完成)

--- my-react/react.js
+++ my-react/react.js
@@ -1,9 +1,11 @@
 const globalData = {
   shouldYield: false, // 控制workLoop暂停或开启
   nextUnitOfWork: null, // 指向WorkLoop要操作的下一个Fiber
+  root: null, // fiber 树的根节点
 };
 
-/** 这是一个适配器模式的应用，抹平了创建不同类型虚拟DOM的差异
+/**
+ * 这是一个适配器模式的应用，抹平了创建不同类型虚拟DOM的差异
  * this function serves as an application of the Adapter Pattern,
  * which reconciles differences in creating various types of visual DOM elements.
  */
@@ -64,11 +66,7 @@ const visualDom2Dom = (visualDom) => {
   };
   const createElement = (visualDom) => {
     const dom = document.createElement(visualDom.type);
-    Object.keys(visualDom.props).forEach((key) => {
-      if (key !== "children") {
-        dom[key] = visualDom.props[key];
-      }
-    });
+    updateProps(dom, visualDom.props);
 
     const { children } = visualDom.props;
     const childVDomList = Array.isArray(children) ? children : [children];
@@ -90,27 +88,81 @@ const visualDom2Dom = (visualDom) => {
 };
 
 /**
- * 所谓渲染，也就是把JSX转为浏览器认识的DOM, 具体的过程是`JSX->Visual DOM->DOM`
- * `render` means that transfer JSX to DOM， and the procession is `JSX->Visual DOM->DOM`.
+ * 1. 所谓渲染，也就是把JSX转为浏览器认识的DOM, 具体的过程是`JSX->Visual DOM->DOM`
+ * 2. 架构调整后，render函数的作用变成了初始化fiber节点
+ * 1.`render` means that transfer JSX to DOM， and the procession is `JSX->Visual DOM->DOM`.
+ * 2. after the adjustment, the role of the render function has transformed into initializing fiber nodes.
  */
 const render = (jsx, container) => {
-  console.log("【render】JSX的数据结构", jsx);
-  const visualDom = jsx2VisualDom(jsx);
-  console.log("【render】虚拟DOM", visualDom);
-  const dom = visualDom2Dom(visualDom);
-  console.log("【render】真实DOM", dom);
+  // console.log("【render】JSX的数据结构", jsx);
+  // const visualDom = jsx2VisualDom(jsx);
+  // console.log("【render】虚拟DOM", visualDom);
+  // const dom = visualDom2Dom(visualDom);
+  // console.log("【render】真实DOM", dom);
 
-  container.appendChild(dom);
+  // container.appendChild(dom);
+  globalData.nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [jsx],
+    },
+  };
+  globalData.root = globalData.nextUnitOfWork;
 };
 
-/** 工作循环，处理异步更新任务，适时渲染界面
+/** TODO:
+ * 更新组件内容，也用到了适配器模式，抹平了函数组件和原生标签的差异
+ * update component, use Adapter Pattern to process the differences between function components and native tags.
+ */
+const updateComponent = (fiber) => {
+  const { type } = fiber;
+  const isFunctionComponent = typeof type === "function";
+
+  const updateFunctionComponent = () => {};
+  const updateHostComponent = () => {};
+
+  if (isFunctionComponent) {
+    updateFunctionComponent();
+  } else {
+    updateHostComponent();
+  }
+};
+
+/** 更新参数 */
+const updateProps = (obj, props) => {
+  Object.keys(props)?.forEach((key) => {
+    if (key !== "children") {
+      obj[key] = props[key];
+    }
+  });
+};
+
+/**
+ * 执行工作循环中的单个任务——对原生标签和函数组件会进行区分处理
+ * perform the atom task of the work loop, and perform different processing for native tags and function components.
+ * */
+const performUnitOfWork = (fiber) => {
+  const { type } = fiber;
+  // const isFunctionComponent = typeof type === "function";
+  updateComponent(fiber);
+  updateProps(fiber.dom, fiber.props);
+  // TODO: 返回下一个兄弟节点或者子节点，以便于继续执行工作循环————当然，这意味着先要建立fiber树结构
+};
+
+/**
+ * 工作循环，处理异步更新任务，适时渲染界面
  *  this function schedules and executes update tasks, rendering UI when the main thread is idle
  */
 const workLoop = (idleDeadLine) => {
   globalData.shouldYield = false;
-  while (!globalData.shouldYield) {
+  while (!globalData.shouldYield && globalData.nextUnitOfWork) {
     console.log("抽空执行~");
+    globalData.nextUnitOfWork = performUnitOfWork(globalData.nextUnitOfWork);
     globalData.shouldYield = idleDeadLine.timeRemaining() > 0;
+  }
+  if (!globalData.nextUnitOfWork && globalData.root) {
+    // TODO：一次loop结束后，需要commit保存本次loop的更新结果
+    console.log("【workLoop】这里需要commit以保存更新结果");
   }
   requestIdleCallback(workLoop);
 };
